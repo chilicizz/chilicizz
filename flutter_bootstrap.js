56fc8f8bfb8489f2d820d5bb4316fff7b50c4354
chilicizz <chilicizz@users.noreply.github.com>
2025-07-01 14:12:36 +0000

    deploy: 2dedca7450d6edf18accef1f062e13ab50be56e6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1008908285"
+    serviceWorkerVersion: "1917920855"
   }
 });
