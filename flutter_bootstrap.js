dac4ff5912b2e0324c115b8d3e593bb7bb80aff8
chilicizz <chilicizz@users.noreply.github.com>
2026-06-22 21:53:45 +0000

    deploy: df8a780d19360433047e5b47ab85667c8af4b0eb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a362","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f8d","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2421705201" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3718602235" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- main.dart.js
+++ main.dart.js
@@ -31931,7 +31931,7 @@ $S:2}
 A.aHW.prototype={
 $1(a){var s=A.dB().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/4c525dac5ebe5971c5708ef73558ed8edcf4a362/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a4ce257c68517c1410f4b48ac9852ab5642a3f8d/":s)+a},
 $S:56}
 A.S9.prototype={
 gv(a){var s=this.a
