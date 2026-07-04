9040f7d142d4bc2f4152b9556bc0cad515067bdd
chilicizz <chilicizz@users.noreply.github.com>
2026-07-04 10:19:03 +0000

    deploy: 7bab23e618fe8a9097b73957af329630e72804d5

--- main.dart.js
+++ main.dart.js
@@ -31931,7 +31931,7 @@ $S:2}
 A.aHW.prototype={
 $1(a){var s=A.dB().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/a4ce257c68517c1410f4b48ac9852ab5642a3f8d/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a10d8ac38de835021c8d2f920dbf50a920ccc030/":s)+a},
 $S:56}
 A.S9.prototype={
 gv(a){var s=this.a
